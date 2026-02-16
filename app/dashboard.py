6d62d53548dd822fe90611f9bcf08b67edd9cf1f
MahanoorIshtiaq <mahanoorishtiaq03@gmail.com>
2026-02-16 23:26:04 +0500

    Update dashboard.py

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,5 +1,11 @@
 # app/dashboard.py 
 
+import sys
+from pathlib import Path
+
+project_root = Path(__file__).parent.parent
+sys.path.insert(0, str(project_root))
+
 import streamlit as st
 import pandas as pd
 import numpy as np
